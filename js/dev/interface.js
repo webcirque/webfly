666423437f796559129c8ed5fb336724c2471edb
Ediel Chedlyramber <15964383+EdChdX@users.noreply.github.com>
2020-07-31 16:48:19 +0800

    Test 'em all!

--- js/dev/interface.js
+++ js/dev/interface.js
@@ -1,9 +1,17 @@
 "use strict";
 
+// Initialize
+var btns = {}, panes = {}, tabs = {};
+
 // Read config
 var getConfig = async function () {
 	var resp = await fetch("../conf/config.json");
 	self.appConfig = await resp.json();
+};
+var getIcons = async function () {
+	var resp = await fetch("../conf/iconidx.json");
+	var iconConfig = await resp.json();
+	self.wimgr = new IconMgr("../img/", iconConfig);
 };
 var setUid = function (uid) {
 	localStorage.setItem("WebFly:userId", uid);
@@ -23,11 +31,50 @@ var startThis = function (gid = appConfig.group.list[appConfig.group.default].id
 		webfly.close();
 	});
 };
+// Refresh all elements on resize
+var uiRefresher = function () {
+	updButtons();
+	eleResize();
+};
+// Force button sizes
+var updButtons = function () {
+	Array.from(document.querySelectorAll("div[id^=btn]")).forEach((e) => {
+		var factor1 = e.children[0];
+		var factor2 = getComputedStyle(factor1);
+		e.style.width = (parseInt(factor1.style.width) + parseInt(factor2.marginLeft) + parseInt(factor2.marginRight)).toString() + "px";
+		e.style.height = (parseInt(factor1.style.height) + parseInt(factor2.marginTop) + parseInt(factor2.marginBottom)).toString() + "px";
+	});
+};
+// Resize elements
+var eleResize = function () {
+	panes.activity.style.width = (innerWidth - panes.drawer.clientWidth).toString() + "px";
+};
 
 // Window
 var drawerDisp, chatDisp;
 document.addEventListener("readystatechange", function () {
 	if (this.readyState == "interactive") {
-
+		getIcons().then(function () {
+			wimgr.updateIconsAll();
+			uiRefresher();
+			addEventListener("resize", function () {
+				eleResize();
+			});
+		});
+		// Get elements
+		// Buttons
+		btns.listDrawer = document.querySelector("#btn-drawer");
+		// Panes
+		panes.drawer = document.querySelector(".disp-drawer");
+		panes.activity = document.querySelector(".disp-activity");
+		// Add event listeners
+		btns.listDrawer.addEventListener("mouseup", function () {
+			if (panes.drawer.className == "disp-drawer expanding style-flex-c") {
+				panes.drawer.className = "disp-drawer inactive-disp";
+			} else {
+				panes.drawer.className = "disp-drawer expanding style-flex-c";
+			};
+			eleResize();
+		});
 	};
 });
